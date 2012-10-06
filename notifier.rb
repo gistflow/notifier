ebef1f599a8e1b48726af311cb5daad7eade8692
Jan Bernacki <releu@me.com>
2012-10-06 18:04:16 +0400

    fix

--- notifier.rb
+++ notifier.rb
@@ -1,9 +1,13 @@
 require 'socket'
+require 'terminal-notifier'
+
+$debug = ENV['TEST'] == 'yep'
 
 Socket.tcp('0.0.0.0', 1666) do |sock|
   sock.puts "123"
   
   while message = sock.gets
+    p [:message, message] if $debug
     TerminalNotifier.notify message, title: "New notification"
   end
 end
